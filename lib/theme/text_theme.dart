54d5eff81d541f4c15bb4a3a90722a553d51ddcc
Abralava Georgii <georgijabralava@gmail.com>
2024-07-26 13:39:20 +0400

    Theme changes

--- lib/theme/text_theme.dart
+++ lib/theme/text_theme.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 
 class TextThemeStyle {
   static TextTheme light = const TextTheme(
-    headlineLarge: TextStyle(fontSize: 96, fontWeight: FontWeight.w600, color: Color.fromRGBO(0, 29, 51, 1)),
+    headlineLarge: TextStyle(fontSize: 72, fontWeight: FontWeight.w600, color: Color.fromRGBO(0, 29, 51, 1)),
     titleLarge: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-    titleMedium: TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: Color.fromRGBO(0, 29, 51, 1)),
-    labelLarge: TextStyle(fontSize: 26, fontWeight: FontWeight.w400, color: Color.fromRGBO(24, 28, 32, 1)),
-    labelMedium: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Color.fromRGBO(81, 96, 111, 1)),
-    bodyLarge: TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: Color.fromRGBO(24, 28, 32, 1)),
-    bodyMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Color.fromRGBO(76, 82, 91, 1)),
-    bodySmall: TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: Color.fromRGBO(76, 82, 91, 1),
+    titleMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Color.fromRGBO(0, 29, 51, 1)),
+    labelLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.w400, color: Color.fromRGBO(24, 28, 32, 1)),
+    labelMedium: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: Color.fromRGBO(81, 96, 111, 1)),
+    labelSmall: TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: Color.fromRGBO(81, 96, 111, 1)),
+    bodyLarge: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Color.fromRGBO(24, 28, 32, 1)),
+    bodyMedium: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Color.fromRGBO(76, 82, 91, 1)),
+    bodySmall: TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: Color.fromRGBO(76, 82, 91, 1),
     ),
   );
 
